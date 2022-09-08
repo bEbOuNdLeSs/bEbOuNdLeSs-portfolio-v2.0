6dee5193229a5c71a09bec6d5b1b57f3ff67b22d
bEbOuNdLeSs <beboundlessssss@gmail.com>
2022-09-07 19:09:09 -0700

    Completeing sidebar and adding layout

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,10 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import './index.scss';
 import Sidebar from '../Sidebar';
 
 const Layout = () => {
-    return <>
-        <Sidebar />
-    </>
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>&lt;/body&gt;</span>
+                <br />
+
+                <span className='bottom-tag-html'>&lt;/body&gt;</span>
+            </div>
+        </div>
+    )
 }
 
 export default Layout;
